bf6c6a84c0a160ea41f47ad2fbfcba63c6843967
asif883 <asifiqbal95863@gmail.com>
2024-11-21 00:11:55 +0600

    basic server setUp

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 4000;
+
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/' , async( req , res ) =>{
+    res.send('server is running')
+})
+
+app.listen(port , ()=>{
+    console.log(`server is running on the port :${port}`);
+    
+})
