e811b16bf6bb47943b78831843c2111614b87dce
asif883 <asifiqbal95863@gmail.com>
2024-11-21 22:35:11 +0600

    verify user

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+  }))
 app.use(express.json())
+
+
+// token verification 
+const verifyJwt =(req, res , next) =>{
+    const authorization = req.headers.authorization
+    if( !authorization ){
+      return res.send({ message: "No token found"})
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token , process.env.ACCESS_TOKEN, (err, decoded) =>{
+      if(err){
+        return res.send({message : "Invalid token"})
+      }
+      req.decoded= decoded;
+      next()
+    })
+  }
+
+ // verify Seller
+
+ const verifySeller = async ( req ,res ,next)=>{
+    const email = req.decoded.email
+    const query = { email : email}
+    const user = await userCollection.findOne(query)
+    if(user?.role !== 'seller'){
+      return res.send({message: "Forbidden access"})
+    }
+    next()
+   }
+
+   
+  
 
 
 // mongoDB
@@ -57,6 +94,64 @@ const client = new MongoClient(uri, {
         const result = await userCollection.find().toArray()
         res.send(result)
     })
+
+    // make admin
+    app.patch('/users/admin/:id' , async ( req , res) =>{
+        const id = req.params.id
+        const filter = { _id: new ObjectId(id) }
+        const updateDoc = {
+            $set: {
+                role: "admin"
+            }
+        }
+        const result = await userCollection.updateOne(filter , updateDoc)
+        res.send(result)
+    })
+
+    // delete 
+    app.delete('/users/:id', async(req , res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result =await userCollection.deleteOne(query);
+        res.send(result);
+      })
+
+     // Add Product
+     app.post('/add-products', verifyJwt, verifySeller, async( req, res )=>{
+        const product = req.body
+        const result = await productCollection.insertOne(product)
+        res.send(result)
+     }) 
+
+     //  all products
+    app.get( '/all-products' , async( req, res )=>{
+        const {title, sort, brand, category} = req.query
+        const query = {}
+  
+        if(title){
+          query.title ={ $regex:title , $options: 'i'}
+        }
+        if(category){
+          query.category ={ $regex:category , $options: 'i'}
+        }
+        if(brand){
+          query.brand = brand
+        }
+  
+        const sortOptions = sort === 'asc' ? -1 : 1
+  
+        const products = await productCollection.find(query).sort({price: sortOptions}).toArray()
+  
+        const productInfo = await productCollection.find({}, { projection:{ category:1 , brand: 1}}).toArray();
+  
+        const totalProduct = await productCollection.countDocuments(query)
+  
+        const brands = [...new Set(productInfo.map((product)=> product.brand ))]
+        const categories = [...new Set(productInfo.map((product)=> product.category ))]
+  
+        res.send({products, brands, categories, totalProduct})
+      })
+  
         
 
     }
@@ -66,6 +161,15 @@ const client = new MongoClient(uri, {
 } 
 
 dbConnect()
+
+// jwt
+ 
+app.post('/authentication', async ( req , res )=>{
+    const userEmail = req.body;
+    const token = jwt.sign(userEmail , process.env.ACCESS_TOKEN, 
+      { expiresIn: '10d'})
+      res.send({token})
+   })
 
 
 app.get('/' , async( req , res ) =>{
